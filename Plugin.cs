a81e8772523c07fa8710e78e7da61a66d2fe8fda
AzumattDev <azumattgaming@gmail.com>
2023-06-17 22:17:42 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using MajesticButton.Utils;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MajesticButton
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class MajesticButtonPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "MajesticButton";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource MajesticButtonLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        internal static GameObject originalButton;
+        internal static List<GameObject> clonedButtons = new();
+        private string currentScene;
+        internal static GameObject buttonParent;
+
+        public void Awake()
+        {
+            ButtonUrls = TextEntryConfig("1 - General", "ButtonUrls", "url1,url2,url3", "Comma-separated list of button URLs. For each URL a button will be created at the main menu next to the 'Show Player.log' button. The button will open the URL in the browser when clicked.");
+            ButtonTexts = TextEntryConfig("1 - General", "ButtonTexts", "Text1,Text2,Text3", "Comma-separated list of button texts. This is the text that is on the button. Defaults to the 'Show Player.log'");
+
+            ButtonUrls.SettingChanged += Config_OnSettingChanged;
+            ButtonTexts.SettingChanged += Config_OnSettingChanged;
+            SceneManager.activeSceneChanged += OnSceneChanged;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= OnSceneChanged;
+            CleanupButtons();
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                MajesticButtonLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                MajesticButtonLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                MajesticButtonLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+        void Config_OnSettingChanged(object sender, EventArgs e)
+        {
+            // Assuming the scene is already loaded when the config changes
+            if (SceneManager.GetActiveScene().name == "start")
+            {
+                CleanupButtons();
+                Functions.UpdateButtons();
+            }
+        }
+
+        private void OnSceneChanged(Scene oldScene, Scene newScene)
+        {
+            // If we leave the start scene, clean up
+            if (oldScene.name == "start")
+            {
+                CleanupButtons();
+                if (buttonParent != null)
+                {
+                    Destroy(buttonParent);
+                    buttonParent = null;
+                }
+            }
+
+
+            // If we enter the start scene, setup buttons
+            if (newScene.name == "start")
+            {
+                originalButton = GameObject.Find("GuiRoot/GUI/StartGui/Menu/showlog");
+                // Add the HorizontalLayoutGroup here.
+                if (originalButton != null)
+                {
+                    // Create a new GameObject as the parent of the buttons.
+                    buttonParent = new GameObject("ButtonParent");
+                    buttonParent.transform.SetParent(originalButton.transform.parent, true);
+                    buttonParent.transform.localPosition = originalButton.transform.localPosition;
+                    if (buttonParent.GetComponent<RectTransform>() == null)
+                    {
+                        var rectTransform = buttonParent.AddComponent<RectTransform>();
+                        rectTransform.sizeDelta = new Vector2(10, 30);
+                    }
+
+                    // Move the original button to the new parent.
+                    originalButton.transform.SetParent(buttonParent.transform, false);
+
+                    // Add the HorizontalLayoutGroup to the new parent.
+                    var horizontalLayoutGroup = buttonParent.AddComponent<HorizontalLayoutGroup>();
+                    horizontalLayoutGroup.spacing = 150; // Set your desired spacing.
+                    horizontalLayoutGroup.childForceExpandWidth = false; // Prevents the buttons from expanding to fill the horizontal space.
+                    horizontalLayoutGroup.childForceExpandHeight = true;
+
+                    Functions.UpdateButtons();
+                }
+            }
+
+            currentScene = newScene.name;
+        }
+
+        private void CleanupButtons()
+        {
+            foreach (GameObject button in clonedButtons.Where(button => button != null))
+            {
+                Destroy(button);
+            }
+
+            clonedButtons.Clear();
+        }
+
+
+        #region ConfigOptions
+
+        internal static ConfigEntry<string> ButtonUrls;
+        internal static ConfigEntry<string> ButtonTexts;
+
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description)
+        {
+            return config(group, name, value, new ConfigDescription(description));
+        }
+
+        internal ConfigEntry<T> TextEntryConfig<T>(string group, string name, T value, string desc,
+            bool synchronizedSetting = true)
+        {
+            ConfigurationManagerAttributes attributes = new()
+            {
+                CustomDrawer = TextAreaDrawer
+            };
+            return config(group, name, value, new ConfigDescription(desc, null, attributes));
+        }
+
+        internal static void TextAreaDrawer(ConfigEntryBase entry)
+        {
+            GUILayout.ExpandHeight(true);
+            GUILayout.ExpandWidth(true);
+            entry.BoxedValue = GUILayout.TextArea((string)entry.BoxedValue, GUILayout.ExpandWidth(true),
+                GUILayout.ExpandHeight(true));
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order;
+            [UsedImplicitly] public bool? Browsable;
+            [UsedImplicitly] public string? Category;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer;
+        }
+
+        #endregion
+    }
+}
---
+++ Utils/Functions.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MajesticButton.Utils;
+
+public class Functions
+{
+    private static void InstantiateButton(string url, string text, int index, GameObject originalButton, Vector3 originalPos, int spacing, GameObject sourceButton, ButtonSfx sourceButtonSfx, ButtonTextColor sourceButtonTextColor, Image sourceImage)
+    {
+        var newButton = GameObject.Instantiate(originalButton, MajesticButtonPlugin.buttonParent.transform);
+
+        UpdateButtonImage(originalButton, sourceImage);
+        AddLayoutElement(originalButton);
+        IncreaseButtonHeight(originalButton);
+        AddContentSizeFitter(originalButton);
+
+        AddClickEvent(newButton, url);
+        AddLayoutElement(newButton);
+        UpdateButtonText(newButton, sourceButton, text);
+        AddComponentButtonSfx(newButton, sourceButtonSfx);
+        AddComponentButtonTextColor(newButton, sourceButtonTextColor);
+        UpdateButtonImage(newButton, sourceImage);
+        IncreaseButtonHeight(newButton);
+        AddContentSizeFitter(newButton);
+
+
+        MajesticButtonPlugin.clonedButtons.Add(newButton);
+    }
+
+
+    private static void AddClickEvent(GameObject button, string url)
+    {
+        button.GetComponent<Button>().onClick = new Button.ButtonClickedEvent(); // Have to fully replace the event, otherwise it keeps the persistent one from the cloned button. onClick.RemoveAllListeners() won't do it.
+        button.GetComponent<Button>().onClick.AddListener(() => { Application.OpenURL(url); });
+    }
+
+    private static void AddLayoutElement(GameObject button)
+    {
+        if (button.GetComponent<LayoutElement>() == null)
+        {
+            var layoutElement = button.AddComponent<LayoutElement>();
+            layoutElement.preferredWidth = button.GetComponent<RectTransform>().sizeDelta.x;
+            layoutElement.preferredHeight = button.GetComponent<RectTransform>().sizeDelta.y;
+        }
+    }
+
+    private static void UpdateButtonText(GameObject button, GameObject sourceButton, string text)
+    {
+        var textComponent = button.GetComponentInChildren<Text>();
+        if (textComponent != null)
+        {
+            textComponent.text = text;
+        }
+        
+        var outlineComponent = button.GetComponentInChildren<Outline>();
+        if (outlineComponent != null)
+        {
+            // Copy values
+            outlineComponent.effectColor = sourceButton.GetComponentInChildren<Outline>().effectColor;
+            outlineComponent.effectDistance = new Vector2(0.75f, 0.75f);
+            outlineComponent.useGraphicAlpha = sourceButton.GetComponentInChildren<Outline>().useGraphicAlpha;
+        }
+        else
+        {
+            var outlineComp = button.GetComponentInChildren<Text>().gameObject.AddComponent<Outline>();
+            // Copy values
+            outlineComp.effectColor = sourceButton.GetComponentInChildren<Outline>().effectColor;
+            outlineComp.effectDistance = new Vector2(0.75f, 0.75f);
+            outlineComp.useGraphicAlpha = sourceButton.GetComponentInChildren<Outline>().useGraphicAlpha;
+        }
+    }
+
+    private static void AddComponentButtonSfx(GameObject button, ButtonSfx source)
+    {
+        if (button.GetComponent<ButtonSfx>() == null)
+        {
+            var newComponent = button.AddComponent<ButtonSfx>();
+            if (source != null && newComponent != null)
+            {
+                newComponent.m_sfxPrefab = source.m_sfxPrefab;
+                newComponent.m_selectable = source.m_selectable;
+                newComponent.m_selectSfxPrefab = source.m_selectSfxPrefab;
+            }
+        }
+    }
+
+    private static void AddComponentButtonTextColor(GameObject button, ButtonTextColor source)
+    {
+        if (button.GetComponent<ButtonTextColor>() == null)
+        {
+            var newComponent = button.AddComponent<ButtonTextColor>();
+            if (source != null && newComponent != null)
+            {
+                newComponent.m_button = newComponent.GetComponent<Button>();
+                newComponent.m_text = newComponent.GetComponentInChildren<Text>();
+                newComponent.m_defaultColor = new Color(1f, 161/255f, 60/255f, 1f);
+
+                newComponent.m_text.color = new Color(1f, 161/255f, 60/255f, 1f);
+
+                newComponent.m_textMesh = source.m_textMesh;
+                newComponent.m_disabledColor = source.m_disabledColor;
+                newComponent.m_defaultMeshColor = source.m_defaultMeshColor;
+            }
+        }
+    }
+
+    private static void UpdateButtonImage(GameObject button, Image source)
+    {
+        var imageComponent = button.GetComponent<Image>();
+        if (source != null && imageComponent != null)
+        {
+            imageComponent.sprite = source.sprite;
+        }
+    }
+
+    private static void IncreaseButtonHeight(GameObject button)
+    {
+        var rectTransform = button.GetComponent<RectTransform>();
+        var size = rectTransform.sizeDelta;
+        size.y *= 1.5f; // Increase the height by 50%
+        rectTransform.sizeDelta = size;
+    }
+
+    private static void AddContentSizeFitter(GameObject button)
+    {
+        if (button.GetComponent<ContentSizeFitter>() == null)
+        {
+            var contentSizeFitter = button.AddComponent<ContentSizeFitter>();
+            contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+        }
+    }
+
+    internal static void UpdateButtons()
+    {
+        var urls = MajesticButtonPlugin.ButtonUrls.Value.Split(',');
+        var texts = MajesticButtonPlugin.ButtonTexts.Value.Split(',');
+        var originalPos = MajesticButtonPlugin.originalButton.transform.localPosition;
+        var spacing = 150;
+
+        var sourceButton = GameObject.Find("GuiRoot/GUI/StartGui/CharacterSelection/SelectCharacter/BottomWindow/Start");
+        var sourceImage = sourceButton?.GetComponent<Image>();
+        var sourceButtonSfx = sourceButton?.GetComponent<ButtonSfx>();
+        var sourceButtonTextColor = sourceButton?.GetComponent<ButtonTextColor>();
+
+        if (urls.Length != texts.Length)
+        {
+            MajesticButtonPlugin.MajesticButtonLogger.LogError("URLs and texts arrays have different lengths.");
+            return;
+        }
+
+        for (int i = 0; i < urls.Length; ++i)
+        {
+            if (i >= urls.Length || string.IsNullOrEmpty(urls[i]))
+            {
+                MajesticButtonPlugin.MajesticButtonLogger.LogWarning($"No URL found for button index {i}, skipping button creation.");
+                continue;
+            }
+
+            InstantiateButton(urls[i], texts[i], i, MajesticButtonPlugin.originalButton, originalPos, spacing, sourceButton, sourceButtonSfx, sourceButtonTextColor, sourceImage);
+        }
+    }
+}
